50fafe5b98d4f4640a5eff3beaebc5c004526412
handahurjui <andahurjui@gmail.com>
2025-04-08 22:00:41 +0300

    add textView delegate; cleanup code and adjust UI display and behaviour;

--- ExpensesManager/ScanViewController.swift
+++ ExpensesManager/ScanViewController.swift
@@ -10,24 +10,44 @@ import VisionKit
 
 class ScanViewController: UIViewController, Storyboarded {
 
-    // MARK: - Properties
+    // MARK: - Outlets
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet private var imageView: UIImageView!
     @IBOutlet weak var scanBtn: UIButton!
     @IBOutlet weak var takePhotoBtn: UIButton!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    
+    //MARK: Properties
+    private let descriptionPlaceHolderText = "Write description of your photo"
     
     // MARK: - View
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Scan"
-           
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow),
-                                               name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide),
-                                               name: UIResponder.keyboardWillHideNotification, object: nil)
+        
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         gestureRecognizer.cancelsTouchesInView = false
         view.addGestureRecognizer(gestureRecognizer)
+        
+        configureUIViews()
+    }
+    
+    private func configureUIViews() {
+        descriptionTextView.layer.cornerRadius = 5
+        descriptionTextView.layer.borderColor = UIColor.black.cgColor
+        descriptionTextView.layer.borderWidth = 1
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        registerForKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unregisterKeyboardNotifications()
     }
     
     // MARK: - Action
@@ -38,13 +58,29 @@ class ScanViewController: UIViewController, Storyboarded {
     }
     
     //MARK: - Methods
+    
+
+    //MARK: Keyboard notifications
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unregisterKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+
+    }
+    
     @objc func keyboardWillShow(notification: NSNotification) {
         if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
             let keyboardHeight = keyboardFrame.height
             scrollView.contentInset.bottom = keyboardHeight
         }
     }
-
+    
     @objc func keyboardWillHide(notification: NSNotification) {
         scrollView.contentInset.bottom = 0
         self.view.endEditing(true)
@@ -55,6 +91,7 @@ class ScanViewController: UIViewController, Storyboarded {
     }
 }
 
+//MARK: - VNDocumentCameraViewControllerDelegate
 extension ScanViewController: VNDocumentCameraViewControllerDelegate {
     func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
         controller.dismiss(animated: true) { [weak self] in
@@ -80,6 +117,25 @@ extension ScanViewController: VNDocumentCameraViewControllerDelegate {
             
             guard let self else { return }
             AlertPresenter.shared.present(title: "Error", message: error.localizedDescription, on: self)
+        }
+    }
+}
+
+//MARK: - UITextViewDelegate
+extension ScanViewController: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        
+        if !descriptionTextView.text!.isEmpty && descriptionTextView.text! == descriptionPlaceHolderText {
+            descriptionTextView.text = ""
+            descriptionTextView.textColor = .black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if descriptionTextView.text.isEmpty {
+            descriptionTextView.text = descriptionPlaceHolderText
+            descriptionTextView.textColor = .lightGray
         }
     }
 }
