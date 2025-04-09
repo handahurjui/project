3e4801ee65b433775dd44a296ee5bed7c7d6097d
handahurjui <andahurjui@gmail.com>
2025-04-10 02:58:58 +0300

    add tableView in HistoryVC instead of SwiftUI List;

--- ExpensesManager/History/ViewController/HistoryViewController.swift
+++ ExpensesManager/History/ViewController/HistoryViewController.swift
@@ -10,11 +10,24 @@ import SwiftUI
 
 class HistoryViewController: UIViewController, Storyboarded {
     
+    @IBOutlet weak var tableView: UITableView!
+    
     //MARK: Properties
-    var viewModel: HistoryViewModel?
+    var viewModel: HistoryViewModel! {
+        didSet {
+            viewModel.viewDelegate = self
+        }
+    }
+    private let cellReuseID = "expenses"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "History"
+        viewModel.loadExpenses()
+//        createSwiftUIList()
+    }
+
+    private func createSwiftUIList() {
         guard let vm = viewModel else { return }
         let expensesHistoryVM = ExpensesHistoryViewModel(storage: vm.storage)
         let listController = UIHostingController(
@@ -24,5 +37,47 @@ class HistoryViewController: UIViewController, Storyboarded {
         view.pinView(listController.view)
         addChild(listController)
         listController.didMove(toParent: self)
+    }
+}
+
+
+//MARK: TableView Delegate & Data Source
+extension HistoryViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.numberOfRows()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseID) ?? UITableViewCell(style: .subtitle, reuseIdentifier: cellReuseID)
+        let cellData = viewModel.cellDataFor(row: indexPath.row)
+        cell.textLabel?.text = cellData.title
+        cell.detailTextLabel?.text = "\(cellData.createdDate)"
+        cell.accessoryType = .disclosureIndicator
+        
+        return cell
+    }
+}
+
+//MARK: TableView Events
+extension HistoryViewController {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        viewModel.didSelectRow(indexPath.row)
+    }
+}
+
+// MARK: - ViewModel Delegate
+extension HistoryViewController: HistoryViewModelDelegate {
+    func refreshScreen() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
+    func showError(errorMessage: String) {
+        DispatchQueue.main.async {
+            AlertPresenter.shared.present(title: "Error",
+                                              message: errorMessage,
+                                              on: self)
+        }
     }
 }
--- ExpensesManager/History/ViewModel/HistoryViewModel.swift
+++ ExpensesManager/History/ViewModel/HistoryViewModel.swift
@@ -5,10 +5,53 @@
 //  Created by Anda on 09.04.2025.
 //
 
+import UIKit
+
+protocol HistoryViewModelDelegate {
+    func refreshScreen()
+    func showError(errorMessage: String)
+}
+
 class HistoryViewModel {
+    
+    
+    // MARK: Properties
     var storage: Storage
+    var managedObjects: [ExpenseContainer]? {
+        didSet {
+            viewDelegate?.refreshScreen()
+        }
+    }
+    
+    var viewDelegate: HistoryViewModelDelegate?
+    var coordinator: HistoryCoordinator?
     
     init(storage: Storage) {
         self.storage = storage
+    }
+    
+    func loadExpenses() {
+        storage.fetchEntry { [weak self] (result) in
+            switch result {
+            case .success(let managedObjects):
+                self?.managedObjects =  managedObjects.compactMap { ExpenseContainer(nsManagedObject: $0)
+                }
+            case .failure(let error):
+                self?.viewDelegate?.showError(errorMessage: error.localizedDescription)
+            }
+        }
+    }
+}
+
+extension HistoryViewModel {
+    // Data Source
+    func numberOfRows() -> Int { return managedObjects!.count }
+    
+    func cellDataFor(row: Int) -> ExpenseModel {
+        return  managedObjects![row].expense!
+    }
+    // Delegate
+    func didSelectRow(_ row: Int) {
+        // GoTo Details Page
     }
 }
